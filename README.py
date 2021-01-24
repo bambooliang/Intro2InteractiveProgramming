0c408958be8b93d0b8e99d9f1bd5800fdf274b71
Scarlet <44127570+bambooliang@users.noreply.github.com>
2021-01-24 15:16:12 -0800

    Create README.py

---
+++ README.py
@@ -0,0 +1,3 @@
+Introduction to Interactive Programming in Python is a online Coursera course that is designed to teach students to learn the basics of building simple interactive applications. 
+
+This Github project includes all the project homework I submitted for this course.
